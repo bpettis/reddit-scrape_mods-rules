3c44cdcf874146e441f082bd4cba8cb278a467f5
Ben Pettis <bpettis@wisc.edu>
2023-01-17 13:10:50 -0600

    output rules to csv

--- get_rules.py
+++ get_rules.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 
-import praw
+import praw, csv
 
-reddit = praw.Reddit("mod_scraper")
+reddit = praw.Reddit("modscraper")
 
 def get_subreddits():
     subreddit_list = []
@@ -11,15 +11,33 @@ def get_subreddits():
         subreddit_list = data.split("\n")
     return subreddit_list
 
+def csv_setup(sub):
+    filename = "output/rules/" + sub + ".csv"
+    with open(filename, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['priority', 'created', 'kind', 'short_name', 'description', 'violation_reason'])
+    print(f'Created {filename} for {sub}')
+    return filename
+
+def get_rules(sub, output):
+    rules = reddit.subreddit(sub).rules
+    for rule in rules:
+        # print(rule)
+        # print(rule.created_utc)
+        # print(rule.kind)
+        # print(rule.description)
+        row = [rule.priority, rule.created_utc, rule.kind, rule.short_name, rule.description, rule.violation_reason]
+        with open(output, 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow(row)
+
 def main():
     print('** get_rules.py | Retrieving Subreddit Rules **')
 
     subreddit_list = get_subreddits()
-
-    print(subreddit_list)
-
-    # subreddit = reddit.subreddit("reddit")
-    # print(subreddit.title)
+    for sub in subreddit_list:
+        output = csv_setup(sub)
+        get_rules(sub, output)
 
     print('** get_rules.py | DONE **')
 
